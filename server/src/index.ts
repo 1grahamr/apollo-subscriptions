5038fe8269b18271c0551c82c57979855ec3f67d
G.Richards <grahamrichards@ustwo.com>
2022-06-23 16:12:04 +0100

    shift to apollo-server-express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,46 +1,50 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'; // websockets needs express
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
 import {typeDefs} from './schema'
-
-const resolvers = {}
 
 const getAccount = (parent: any, args: any, context: any, info: any) => {
     console.log(`getAccount resolver`, {parent, args, context, info})
     return {
         id: "unknown",
         tag: args.tag,
-        balance: 0,
+        balance: 100,
     }
 }
 
-const server = new ApolloServer({
-    typeDefs,
-
-    resolvers: {
-        Query: {
-          getAccount,
-        },
-      },
-
-  csrfPrevention: true,  // highly recommended
-
-  cache: 'bounded',
-//   playground: {
-//     settings: {
-//       'editor.theme': 'light',
-//     },
-//     tabs: [
-//       {
-//       },
-//     ],
-//   },
-});
-
-
-server.listen().then(() => {
-    console.log(`
-      🚀  Server is running!
-      🔉  Listening on port 4000
-      📭  Query at https://studio.apollographql.com/dev
-  `);
-  });
+async function startApolloServer(typeDefs: any, resolvers: any) {
+    const app = express()
+    const httpServer = http.createServer(app)
   
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      csrfPrevention: true,
+      cache: 'bounded',
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    })
+
+    await server.start()
+
+    server.applyMiddleware({ app })
+
+    await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
+
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  }
+
+const main = async () => {
+    const resolvers = {
+            Query: {
+                getAccount,
+            },
+      }
+    await startApolloServer(typeDefs, resolvers )
+}
+
+main()
+.catch((e) => {
+    throw e
+})
+
