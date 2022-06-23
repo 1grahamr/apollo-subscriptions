d3d755f1ab53053efc3511580ae8951649de9a4d
G.Richards <grahamrichards@ustwo.com>
2022-06-23 16:21:35 +0100

    add websocket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import { ApolloServer } from 'apollo-server-express'; // websockets needs express
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
 import {typeDefs} from './schema'
+
+import { createServer } from 'http';
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 const getAccount = (parent: any, args: any, context: any, info: any) => {
     console.log(`getAccount resolver`, {parent, args, context, info})
@@ -15,15 +20,43 @@ const getAccount = (parent: any, args: any, context: any, info: any) => {
 
 async function startApolloServer(typeDefs: any, resolvers: any) {
     const app = express()
+    
     const httpServer = http.createServer(app)
   
+    const schema = makeExecutableSchema({ typeDefs, resolvers })
+
     const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-      csrfPrevention: true,
-      cache: 'bounded',
-      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+        schema,
+        csrfPrevention: true,
+        cache: "bounded",
+        plugins: [
+          // Proper shutdown for the HTTP server.
+          ApolloServerPluginDrainHttpServer({ httpServer }),
+    
+          // Proper shutdown for the WebSocket server.
+          {
+            async serverWillStart() {
+              return {
+                async drainServer() {
+                  await serverCleanup.dispose();
+                },
+              };
+            },
+          },
+        ],
+      });
+
+    // Creating the WebSocket server
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+        // Pass a different path here if your ApolloServer serves at
+        // a different path.
+        path: '/graphql',
     })
+
+    // Hand in the schema we just created and have the
+    // WebSocketServer start listening.
+    const serverCleanup = useServer({ schema }, wsServer)
 
     await server.start()
 
